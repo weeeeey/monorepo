1f72a466e50fd9bd14835fa2781b8acf194d4a34
위영진 <qser155@naver.com>
2024-12-11 18:12:37 +0900

    feat(useScrollDirection): 스크롤 방향을 알려주는 커스텀 훅 생성

---
+++ packages/shared-hooks/hooks/useScrollDirecton.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState, useRef } from "react";
+import { throttle } from "lodash";
+
+interface ReturnType {
+  isDown: boolean;
+}
+
+/**
+ *  스크롤 방향을 감지하는 커스텀 훅
+ * @param throttleTime
+ * @returns {
+ *   isDown: boolean;
+ * }
+ */
+export const useScrollDirection = (throttleTime = 200): ReturnType => {
+  const [isDown, setIsDown] = useState(false);
+  const prevScrollRef = useRef(0);
+
+  useEffect(() => {
+    const handleScroll = throttle(() => {
+      const curScroll = window.scrollY;
+      if (curScroll > prevScrollRef.current) setIsDown(true);
+      if (curScroll < prevScrollRef.current) setIsDown(false);
+      prevScrollRef.current = curScroll;
+    }, throttleTime); // 200ms 간격으로 스크롤 이벤트 처리
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollRef]);
+
+  return { isDown };
+};
