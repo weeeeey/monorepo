59ecfa8a7840445b47d1ffc6d821bb5352077036
위영진 <qser155@naver.com>
2024-12-11 12:53:09 +0900

    Initial commit

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,16 @@
+import config from '@repo/tailwind-config';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    ...config,
+    content: [
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            ...config.theme.extend,
+        },
+    },
+    plugins: [],
+};
---
+++ packages/tailwind-config/index.ts
@@ -0,0 +1,58 @@
+import type { Config } from 'tailwindcss';
+import animate from 'tailwindcss-animate';
+
+const config: Omit<Config, 'content'> = {
+    darkMode: ['class'],
+    // 각 앱에서 개별적으로 content 경로 지정하도록 비워두는게 좋지만 일단은 이렇게
+    theme: {
+        extend: {
+            width: {
+                zeroGym: '24rem',
+            },
+
+            colors: {
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))',
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))',
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))',
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))',
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))',
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))',
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))',
+                },
+            },
+            borderRadius: {
+                lg: `var(--radius)`,
+                md: `calc(var(--radius) - 2px)`,
+                sm: 'calc(var(--radius) - 4px)',
+            },
+        },
+    },
+    plugins: [animate],
+};
+
+export default config;
